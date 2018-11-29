fc552259923039f609d960edebc3c4f9145d6b14
ThanhTeko <thanh.pd@teko.vn>
2018-11-29 14:20:22 +0700

    fix: menu options

--- src/editor/Menu.js
+++ src/editor/Menu.js
@@ -9,25 +9,29 @@ type Props = {
     top: number,
     left: number,
     title: string,
+    data: object,
 }
 
 export default class Menu extends React.Component<Props>{
     render() {
+        let {data} = this.props;
+        data = data || {};
+
         return (
             <div id="select-element-menu" style={{top: this.props.top, left: this.props.left}}>
                 <h1 className="select-element-menu-handle" id="element-tagName">{this.props.title}</h1>
-                <li className="slider-settings">Slider Settings</li>
                 <li className="edit-element subContent">
                     Edit
                     <div className="arrow">
                         <div/>
                     </div>
                     <div className="subMenu">
-                        <span className="subMenuText edit-text" onClick={this.props.onEditTextMenuClick}>Edit Text</span>
+                        {!!this.shouldShowEditText(data) && <span className="subMenuText edit-text"
+                                                                onClick={this.props.onEditTextMenuClick}>Edit Text</span>}
                         <span className="subMenuText edit-attr">Edit Attribute</span>
-                        <span className="subMenuText change-image">Edit Image</span>
-                        <span className="subMenuText edit-hyper-link">Edit Hyperlink</span>
-                        <span className="subMenuText make-hyper-link">Add Hyperlink</span>
+                        {!!this.shouldShowEditImage(data) && <span className="subMenuText change-image">Edit Image</span>}
+                        {!!data.editHyperLink && <span className="subMenuText edit-hyper-link">Edit Hyperlink</span>}
+                        {!!data.editHyperLink && <span className="subMenuText make-hyper-link">Add Hyperlink</span>}
                     </div>
                 </li>
                 <li className="insert-element subContent">
@@ -44,22 +48,16 @@ export default class Menu extends React.Component<Props>{
                 <li className="element-move" >Move/Resize</li>
                 <li className="element-rearrange">Rearrange Element</li>
                 <li className="element-remove">Remove Element</li>
-                <li id="element-children" className="subContent">
-                    <span>Select Child</span>
-                    <div className="arrow">
-                        <div/>
-                    </div>
-                    <div className="subMenu"/>
-                </li>
-                <li id="element-parents" className="subContent">
-                    <span>Select Parent</span>
-                    <div className="arrow">
-                        <div/>
-                    </div>
-                    <div className="subMenu"/>
-                </li>
                 <li className="select-element-menu-close" onClick={this.props.onCloseMenuClick}>Close Menu</li>
             </div>
         );
+    }
+
+    shouldShowEditImage(data) {
+        return data.imgSrc !== undefined;
+    }
+
+    shouldShowEditText(data) {
+        return !((data.children && data.children.length > 0) || data.tagName === 'img' || data.tagName === 'input');
     }
 }
