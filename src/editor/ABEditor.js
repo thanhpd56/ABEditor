d9ac45a0119229b258bb653193158826d5a4473f
ThanhTeko <thanh.pd@teko.vn>
2018-11-29 10:40:19 +0700

    add: move, remove action

--- src/editor/ABEditor.js
+++ src/editor/ABEditor.js
@@ -1,13 +1,212 @@
 // @flow
-import React from 'react';
+import React, {Fragment} from 'react';
+import $ from 'jquery';
+import pm from 'post-robot';
+import './css/actionBuilder.css';
+import './css/elementHandler.css';
+import Menu from './Menu';
+import Draggable from 'react-draggable';
 
-export default class ABEditor extends React.Component{
+type Props = {}
+
+type State = {}
+
+export default class ABEditor extends React.Component<Props, State> {
+
+    camp = {
+        id: 1
+    };
+
+    customizationTypes = {
+        'css': {'function': 'setCssSettings'},
+        'click': {'function': 'setClickSettings'},
+        'remove': {'function': 'removeElement', 'closeMenu': true},
+        'changedText': {'function': 'setChangedText'},
+        'changedImage': {'function': 'setChangedImage'},
+        'insertedHTML': {'function': 'setInsertHTML'},
+        'insertImage': {'function': 'setInsertImage'},
+        'editHyperLink': {'function': 'setEditHyperLink'},
+        'makeHyperLink': {'function': 'setMakeHyperLink'},
+        'elementAttr': {'function': 'setElementAttr'},
+        'slide': {'function': 'setSlideSettings'},
+        'rearrangeElement': {'function': 'setRearrangePosition'},
+        'moveElement': {'function': 'setMovePosition'}
+    };
+
+    pmTarget = null;
+
+    constructor(props) {
+        super(props);
+        $(document).ready(() => {
+            this.pmTarget = $('#edit-frame')[0].contentWindow;
+            this.initElements();
+            this.setupListeners();
+            pm.send(this.pmTarget, 'setEditMode');
+        });
+
+        this.state = {
+            showMenu: false,
+            showOverlay: false,
+            menuPositionTop: 0,
+            menuPositionLeft: 0,
+            customizationList: [],
+
+        };
+    }
+
+    onOverlayClick = () => {
+        this.closeElementMenu();
+    };
+
+    setupListeners = () => {
+        pm.on('elementSelected', event => {
+            const selectedElement = event.data;
+            this.setState({selectedElement, showOverlay: true});
+        });
+
+        pm.on('setCustomMenu', event => {
+            console.log('show menu');
+            const selectedElement = event.data;
+            this.setCustomMenu(selectedElement);
+        });
+
+        pm.on('removePageOverlay', () => this.removeOverlay());
+
+        pm.on('removeMenu', this.removeMenu);
+
+        pm.on('setElementMovedPosition', (event) => {
+            return this.setElementMovedPosition(event.data);
+        });
+    };
+
+    setCustomMenu = (data) => {
+        console.log('show menu');
+        const {x, y} = data.position;
+        const {top, left} = data.offset;
+        this.setState({
+            showMenu: true,
+            menuPositionTop: top - x,
+            menuPositionLeft: left - y,
+        });
+    };
+
+    initElements = () => {
+        this.clickEvents();
+    };
+    clickEvents = () => {
+        const eventWrapper = $('body');
+
+        eventWrapper.on('click', '.select-element-menu-close', () => {
+            this.closeElementMenu();
+        });
+
+        eventWrapper.on('click', '.element-move', () => {
+            this.moveElement();
+        });
+
+        eventWrapper.on('click', '.element-remove', () => {
+            this.removeElement();
+        });
+
+    };
+
+    moveElement = () => {
+        pm.send(this.pmTarget, 'prepareElementForMove', this.state.selectedElement);
+    };
+
+    setElementMovedPosition = (data) => {
+        data.type = 'moveElement';
+        this.setChange(data);
+    };
+
+    setChange = (customization) => {
+        const selectedElement = this.state.selectedElement;
+        customization.selectorString = selectedElement.selectorString;
+        customization.id = this.createElementClass();
+        customization.parentNodeSelector = selectedElement.parentNodeSelector;
+        customization.parentNodeHTML = selectedElement.parentNodeHTML;
+
+        this.applyCustomization(customization, 'new');
+    };
+
+    createElementClass() {
+        return 'sp-custom-' + this.camp.id + '-' + new Date().getTime();
+    }
+
+    getCustomizationSettings = (type) => {
+        return this.customizationTypes[type];
+    };
+
+    applyCustomization = (customization, changeType, customIndex) => {
+        const setting = this.getCustomizationSettings(customization.type);
+
+        if (typeof setting === 'undefined') {
+            return false;
+        }
+
+        pm.send(this.pmTarget, setting.function, customization).then(data => {
+            if (changeType === 'new') {
+                this.state.customizationList.push(data);
+            }
+            else if (changeType === 'update') {
+                this.state.customizationList[customIndex] = data;
+            }
+
+            if (typeof setting.closeMenu === 'undefined' || setting.closeMenu === true) {
+                this.closeElementMenu();
+            }
+        }).catch(err => {
+            console.log('that bai roi');
+
+        });
+
+    };
+
+    closeElementMenu = () => {
+        this.removeMenu();
+        this.removeOverlay();
+    };
+
+
+    removeMenu = () => {
+        this.setState({
+            showMenu: false,
+        });
+    };
+
+    removeOverlay = () => {
+        this.setState({
+            showOverlay: false,
+        });
+        pm.send(this.pmTarget, 'removeElemOverlay');
+    };
+
+    removeElement = () => {
+        const data = {};
+        data.type = 'remove';
+
+        this.setChange(data);
+    };
+
 
     render() {
+        console.log(this.state.showOverlay);
         return (
-            <div>
-                <iframe id="edit-frame" src="http://localhost:3000" style={{width: '100%', height: '850px'}}></iframe>
-            </div>
+            <Fragment>
+                <div onClick={this.onOverlayClick}>
+                    {this.state.showMenu && <Draggable>
+                        <Menu
+                            onCloseMenuClick={this.closeElementMenu}
+                            onMoveMenuClick={this.moveElement}
+                            onRemoveMenuClick={this.removeElement}
+                            top={this.state.menuPositionTop}
+                            left={this.state.menuPositionLeft}
+                        />
+                    </Draggable>}
+                    {this.state.showOverlay && <div id="selectElementOverlay"/>}
+                    <iframe id="edit-frame" src="http://localhost:3000" style={{width: '100%', height: '850px'}}/>
+                </div>
+            </Fragment>
         );
     }
 
---
+++ src/editor/Menu.js
@@ -0,0 +1,64 @@
+// @flow
+import React from 'react';
+
+type Props = {
+    onCloseMenuClick: Function,
+    onMoveMenuClick: Function,
+    onRemoveMenuClick: Function,
+    top: number,
+    left: number,
+}
+
+export default class Menu extends React.Component<Props>{
+    render() {
+        return (
+            <div id="select-element-menu" style={{top: this.props.top, left: this.props.left}}>
+                <h1 className="select-element-menu-handle" id="element-tagName"/>
+                <li className="slider-settings">Slider Settings</li>
+                <li className="edit-element subContent">
+                    Edit
+                    <div className="arrow">
+                        <div/>
+                    </div>
+                    <div className="subMenu">
+                        <span className="subMenuText edit-text">Edit Text</span>
+                        <span className="subMenuText edit-attr">Edit Attribute</span>
+                        <span className="subMenuText change-image">Edit Image</span>
+                        <span className="subMenuText edit-hyper-link">Edit Hyperlink</span>
+                        <span className="subMenuText make-hyper-link">Add Hyperlink</span>
+                    </div>
+                </li>
+                <li className="insert-element subContent">
+                    Add
+                    <div className="arrow">
+                        <div/>
+                    </div>
+                    <div className="subMenu">
+                        <span className="subMenuText insert-html">Add HTML</span>
+                        <span className="subMenuText insert-image">Add Image</span>
+                    </div>
+                </li>
+                <li className="edit-style">Edit Style</li>
+                <li className="element-move" onClick={this.props.onMoveMenuClick}>Move/Resize</li>
+                <li className="element-rearrange">Rearrange Element</li>
+                <li className="element-remove" onClick={this.props.onRemoveMenuClick}>Remove Element</li>
+                <li id="element-children" className="subContent">
+                    <span>Select Child</span>
+                    <div className="arrow">
+                        <div/>
+                    </div>
+                    <div className="subMenu"/>
+                </li>
+                <li id="element-parents" className="subContent">
+                    <span>Select Parent</span>
+                    <div className="arrow">
+                        <div/>
+                    </div>
+                    <div className="subMenu"/>
+                </li>
+                <li className="select-element-menu-close" onClick={this.props.onCloseMenuClick}>Close Menu</li>
+            </div>
+        );
+    }
+
+}
